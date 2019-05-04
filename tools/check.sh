5c863301012af7ea5e8009883505fd0968102ade
Jesse Connell <jesse08@gmail.com>
2019-05-04 12:15:13 -0400

    update check script, hook into pre-commit for git

---
+++ tools/check.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Javascript Lint
+function check_javascript_all {
+	(
+	set -o errexit -o pipefail
+	# First, plain ol' javascript
+	find . -name '*.js' -a ! -name 'js-instafeed.js' -exec jshint {} +
+	# Separately handle liquid-templated-javascript with a kludgy bit of
+	# sed to hide it from jshint
+	find . -name '*.js.liquid' | while read fn; do
+		jshint <(sed "s/{{.*}}/0/" "$fn") | sed -r "s:^/dev/fd/[0-9]+:$fn:"
+	done
+	)
+}
+
+# Liquid lint
+function check_liquid_all {
+	stash=$(mktemp -d)
+	lintout=$(
+	set -o errexit -o pipefail
+	# Wrapper for liquid-linter than strips out the unrecognized "-"
+	# characters shopify uses to control whitespace.
+	find . -iname '*.liquid' -exec rsync -rR {} $stash/ \;
+	find "$stash" -type f -exec sed -i 's/{%-/{%/g;s/-%}/%}/' {} \;
+	liquid-linter \
+		--custom-tag layout \
+		--custom-tag form \
+		--custom-tag endform \
+		--custom-tag paginate \
+		--custom-tag endpaginate \
+	       "$stash" |& sed "s:$stash:.:" | grep -v ': no issues found$'
+	)
+	if [[ $stash != "" ]]; then
+		rm -rf "$stash"
+	fi
+	if [[ $lintout != "" ]]; then
+		echo "$lintout"
+		return 1
+	fi
+}
+
+function check_main {
+	check_javascript_all
+	check_liquid_all
+}
+
+if [[ ${FUNCNAME[0]} == main ]]; then
+	check_main
+fi
