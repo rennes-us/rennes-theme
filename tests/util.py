8f25f09e0801b994936d759c5d5ac1300ceefd55
Jesse Connell <jesse08@gmail.com>
2020-01-12 23:13:08 +0000

    live tests: support env var for settings JSON

--- tests/util.py
+++ tests/util.py
@@ -6,33 +6,46 @@ import os
 import logging
 import json
 import unittest
+import base64
 import yaml
 # https://stackoverflow.com/a/8910326/4499968
 from xvfbwrapper import Xvfb
 
-try:
-    with open("config.yml") as f_in:
-        CONFIG = yaml.safe_load(f_in)
-except FileNotFoundError:
-    CONFIG = {}
+def __load_theme_config():
+    try:
+        with open("config.yml") as f_in:
+            config = yaml.safe_load(f_in)
+    except FileNotFoundError:
+        config = {}
+    return config
 
-with open("config/settings_data.json") as f_in:
-    SETTINGS = json.loads(f_in.read())
+def __load_settings_data():
+    try:
+        with open("config/settings_data.json") as f_in:
+            settings = json.loads(f_in.read())
+    except FileNotFoundError:
+        txt = os.getenv("SHOPIFY_SETTINGS_DATA")
+        if txt is None:
+            raise RuntimeError(
+                "config/settings_data.json not found; "
+                "define SHOPIFY_SETTINGS_DATA with "
+                "base64-encoded configuration instead.")
+        settings = json.loads(base64.b64decode(txt))
+    return settings
 
-TESTING_CONFIG = {
-    "store_site": CONFIG.get("development", {}).get("store") or os.getenv("SHOPIFY_STORE"),
-    "store_password": os.getenv("SHOPIFY_STORE_PASSWORD"),
-    "elem_delay": float(os.getenv("SHOPIFY_TEST_DELAY", "0")),
-    "real_x11": os.getenv("SHOPIFY_TEST_SHOW") is not None,
-    "log_level": int(os.getenv("SHOPIFY_TEST_LOGLEVEL", "30")),
-    # Seems like we sometimes get banned temporarily, probably from hammering
-    # instagram's server too hard.
-    "check_instafeed": os.getenv("SHOPIFY_CHECK_INSTA", "True").title() == "True",
-    "page_load_timeout": 90000 # ms?
-    }
-
-logging.basicConfig(level=TESTING_CONFIG["log_level"])
-LOGGER = logging.getLogger(__name__)
+def __setup_testing_config(config):
+    testing_config = {
+        "store_site": config.get("development", {}).get("store") or os.getenv("SHOPIFY_STORE"),
+        "store_password": os.getenv("SHOPIFY_STORE_PASSWORD"),
+        "elem_delay": float(os.getenv("SHOPIFY_TEST_DELAY", "0")),
+        "real_x11": os.getenv("SHOPIFY_TEST_SHOW") is not None,
+        "log_level": int(os.getenv("SHOPIFY_TEST_LOGLEVEL", "30")),
+        # Seems like we sometimes get banned temporarily, probably from hammering
+        # instagram's server too hard.
+        "check_instafeed": os.getenv("SHOPIFY_CHECK_INSTA", "True").title() == "True",
+        "page_load_timeout": 90000 # ms?
+        }
+    return testing_config
 
 def __log_testing_config():
     for key in TESTING_CONFIG:
@@ -40,6 +53,13 @@ def __log_testing_config():
         if key == "store_password" and val:
             val = "********"
         LOGGER.info("config: %s=%s", key, val)
+
+CONFIG = __load_theme_config()
+SETTINGS = __load_settings_data()
+TESTING_CONFIG = __setup_testing_config(CONFIG)
+
+logging.basicConfig(level=TESTING_CONFIG["log_level"])
+LOGGER = logging.getLogger(__name__)
 __log_testing_config()
 
 TEST_PRODUCTS = {
