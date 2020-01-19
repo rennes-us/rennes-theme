0b0784bf038c4d1aef45b84e917d013bc5013148
Jesse Connell <jesse08@gmail.com>
2020-01-19 17:27:12 +0000

    live tests: docstrings

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,10 @@ Test suite for a browser interacting with the development site.
 
 This ensures that the development site running on Shopify behaves as expected,
 referencing the config/settings_data.json here (and assuming that is the same
-as Shopify's copy).
+as Shopify's copy).  I'm working on making this flexible enough to test against
+the live theme too, but it's not quite ready yet.  I'd like to let it test the
+alternate testing theme too but I can't see an easy way to do that without
+having the browser authenticate as a site admin.
 
 This package uses Selenium to automate a locally-running web browser (currently
 hardcoded as Chrome).  If the store is password-protected, the environment
