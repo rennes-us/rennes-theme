6cda7f463782340a3da372f89e1772cae4a20302
Jesse Connell <jesse08@gmail.com>
2020-02-17 01:38:53 +0000

    live tests: reorg collections, test empty case

--- tests/test_site.py
+++ tests/test_site.py
@@ -8,6 +8,7 @@ test discovery.  See util.main for that part.
 
 import logging
 import unittest
+import re
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -120,31 +121,6 @@ class TestSite(StoreSite):
         self.check_nav_product()
         self.check_snippet_collection()
         self.check_pagination()
-
-    def test_template_collection_submenu(self):
-        """A collection page for a collection that is with in another category."""
-        self.get("collections/skirts")
-        self.check_layout()
-        self.check_header()
-        self.check_instafeed()
-        self.check_snippet_address()
-        self.check_snippet_mailing_list()
-        self.check_nav_site()
-        self.check_nav_product(clothing_menu_starts="block")
-        self.check_snippet_collection(paginate=False)
-
-    @unittest.skip("not yet implemented")
-    def test_template_collection_designers(self):
-        """The collection page for the list of designers."""
-        self.get("collections/designers")
-        self.check_layout()
-        self.check_header()
-        self.check_instafeed()
-        self.check_snippet_address()
-        self.check_snippet_mailing_list()
-        self.check_nav_site()
-        self.check_nav_product()
-        self.check_snippet_collection_designers()
 
     @unittest.skip("not yet implemented")
     def test_template_gift_card(self):
@@ -402,6 +378,59 @@ class TestSiteProducts(StoreSite):
         self.check_decoration_on_hover(link1, "underline", "underline")
         link2 = self.check_for_elem("p/a", proddesc)
         self.check_decoration_on_hover(link2, "underline", "underline")
+
+
+class TestSiteCollections(StoreSite):
+    """Test suite for store - collection cases.
+
+    This checks various specific cases for collections pages.  For some of
+    these it uses a set of special collections prefixed with "testing," and
+    will skip those tests if the collections are not present.  See
+    TestSiteProducts for the per-product equivalent.
+    """
+
+    def test_template_collection_submenu(self):
+        """A collection page for a collection that is with in another category."""
+        self.get("collections/skirts")
+        self.check_layout()
+        self.check_header()
+        self.check_instafeed()
+        self.check_snippet_address()
+        self.check_snippet_mailing_list()
+        self.check_nav_site()
+        self.check_nav_product(clothing_menu_starts="block")
+        self.check_snippet_collection(paginate=False)
+
+    @unittest.skip("not yet implemented")
+    def test_template_collection_designers(self):
+        """The collection page for the list of designers."""
+        self.get("collections/designers")
+        self.check_layout()
+        self.check_header()
+        self.check_instafeed()
+        self.check_snippet_address()
+        self.check_snippet_mailing_list()
+        self.check_nav_site()
+        self.check_nav_product()
+        self.check_snippet_collection_designers()
+
+    def test_template_collection_empty(self):
+        """An empty collection should just show some placeholder text."""
+        self.get("collections/testing-empty")
+        if self.is404():
+            self.skipTest("testing-empty collection not available")
+        self.check_layout()
+        self.check_header()
+        self.check_instafeed()
+        self.check_snippet_address()
+        self.check_snippet_mailing_list()
+        self.check_nav_site()
+        self.check_nav_product()
+        # Make sure we get the expected text for an empty collection, but
+        # disregard whitespace.
+        self.assertEqual(
+            re.sub(r"\s", "", self.xp("//article[@class='products']").text),
+            re.sub(r"\s", "", get_setting("collection_empty_text")))
 
 
 class TestSiteMailingList(StoreSite):
