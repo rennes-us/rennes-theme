6b24c95f071daf63d561774b5a94e864e65615b0
Jesse Connell <jesse08@gmail.com>
2021-01-30 18:12:12 +0000

    tools: download: run rsync check on download files

--- tools/download.sh
+++ tools/download.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # download all theme files currently on shopify for each known environment.
 # Must be run in the top level of the repo.
-# Then, you can do somethig like rsync -nirc download/development/ ./ and see
-# how it looks.
 
 cat config.yml | grep '^[^ #]' | cut -f 1 -d : | while read environ; do
 	dir="download/$environ"
 	echo "$dir"
 	mkdir -p "$dir"
 	theme download -e $environ -d "$dir"
+done
+for dir in download/*; do
+	echo $dir
+	rsync -nirc $dir/ ./
 done
